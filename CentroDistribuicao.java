b34931d6dc3da85cdca156f3d32612dd125dd89b
icaro espadim <10087971@pucrs.br>
2022-09-27 19:28:38 -0300

    codigo

--- CentroDistribuicao.java
+++ CentroDistribuicao.java
@@ -11,6 +11,7 @@ public class CentroDistribuicao {
     public static final int MAX_ALCOOL = 2500;
     public static final int MAX_GASOLINA = 10000;
 
+    //O método construtor recebe as quantidades iniciais de gasolina nos tanques e ajusta a “situação” de acordo. Caso algum dos parâmetros tenha valor inválido o método deve gerar uma ILLEGAL_ARGUMENT_EXCEPTION (isso vale também para quantidades iniciais de álcool que devem ser iguais).
     public CentroDistribuicao(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
     }
 
@@ -32,6 +33,7 @@ public class CentroDistribuicao {
     public int gettAlcool2() {
     }
 
+    //Os métodos “recebeAditivo”, “recebeGasolina” e “recebeAlcool” são usados quando o centro de distribuição recebe carga dos componentes. Todos recebem por parâmetro a quantidade do componente (aditivo, gasolina ou álcool) recebida e retornam à quantidade que puderam armazenar devido a limitação do tamanho dos tanques e de quanto ainda tinham armazenado. Devem retornar “-1” caso a quantidade recebida por parâmetro seja inválida.
     public int recebeAditivo(int qtdade) {
     }
 
@@ -41,6 +43,7 @@ public class CentroDistribuicao {
     public int recebeAlcool(int qtdade) {
     }
 
+    
     public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
     }
 }
